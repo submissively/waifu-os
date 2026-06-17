5b19f239b5c303ec66a4df71d2ef492c2dbef7b2
submissively <oliviaekitten@proton.me>
2026-06-17 17:59:56 -0400

    Update home.js

--- src/programs/internet_pages/home.js
+++ src/programs/internet_pages/home.js
@@ -27,6 +27,7 @@ I created this site to piss off some people lol!
 <i>"im proud of you!"</i> - 0day<br>
 <i>"Purge, your site is radical!"</i> - MR. Robot<br>
 <i>"Very cool effects, too bad it takes so long to load sob"</i> - Kl0ps
+<i>"This is peak"</i> - GunGunson
 </td>
 </tr>
 </table>
